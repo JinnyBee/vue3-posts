2d882d6e0a3ff56e12245096cc557ffb60c04d72
jinnyBee <hjjeon81@gmail.com>
2024-06-19 16:25:33 +0900

    사용하지 않는 import문 삭제

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,7 +25,7 @@ export default {
 </script>
 
 <script setup>
-import { reactive, ref, toRef, toRefs } from 'vue';
+import { reactive, ref, toRefs } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
